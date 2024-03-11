0744f5899e27aa113db26ddd21e3c6385e17358c
adityaarun2 <67866082+adityaarun2@users.noreply.github.com>
2024-03-11 16:49:40 -0700

    Update script.js

--- script.js
+++ script.js
@@ -9,49 +9,49 @@ const svg = d3.select("#bar-chart")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 // Create a tooltip
 var tooltip = d3.select("body").append("div")
     .attr("class", "tooltip")
     .style("opacity", 0);
+    
 
 // Parse the Data
 d3.json('total_world_cup_goals_by_country.json').then(function(data) {
 
-  // Sort the data by total goals
-  data.sort(function(a, b) { return d3.descending(a.total_goals, b.total_goals); });
+  // Sort the data by total goals and take the top 10
+  const top10Data = data.sort((a, b) => d3.descending(a.total_goals, b.total_goals)).slice(0, 10);
 
   // X axis
   const x = d3.scaleLinear()
-    .domain([0, d3.max(data, function(d) { return d.total_goals; })])
+    .domain([0, d3.max(top10Data, d => d.total_goals)])
     .range([0, width]);
-  svg.append("g")
+    svg.append("g")
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(x))
     .selectAll("text")
-      .style("text-anchor", "end")
-      .attr("dx", "-.8em")
-      .attr("dy", ".15em")
-      .attr("transform", "rotate(-90)");
+      .style("text-anchor", "middle") // Change text-anchor to middle
+      .attr("dx", "0em") // Remove the dx shift
+      .attr("dy", "1em") // Adjust vertical spacing to move text below ticks
+      .attr("transform", ""); // Remove the rotate transformation
 
   // Y axis
   const y = d3.scaleBand()
     .range([0, height])
-    .domain(data.map(function(d) { return d.country; }))
+    .domain(top10Data.map(d => d.country))
     .padding(0.1);
   svg.append("g")
     .call(d3.axisLeft(y));
 
   // Bars
   svg.selectAll(".bar")
-    .data(data)
+    .data(top10Data)
     .enter().append("rect")
     .attr("class", "bar")
-    .attr("x", x(0) )
-    .attr("y", function(d) { return y(d.country); })
-    .attr("width", function(d) { return x(d.total_goals); })
+    .attr("x", x(0))
+    .attr("y", d => y(d.country))
+    .attr("width", d => x(d.total_goals))
     .attr("height", y.bandwidth())
     .attr("fill", "#69b3a2")
     .on("mouseover", function(event, d) {
@@ -67,4 +67,13 @@ d3.json('total_world_cup_goals_by_country.json').then(function(data) {
         .duration(500)
         .style("opacity", 0);
     });
+
+
+    svg.append("text")
+    .attr("x", (width / 2))             
+    .attr("y", height + margin.bottom/ 1.3)  // Adjust this to position the text below the chart
+    .attr("text-anchor", "middle")  
+    .style("font-size", "15px") 
+    .style("font-weight", "bold")
+    .text("Top 10 countries based on their total goals in the World Cup.");
 });
